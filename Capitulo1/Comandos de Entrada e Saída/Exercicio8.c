941cfb53575ac878686aeaad67cea44e57be540e
Leonardo Oliveira <110206541+ILeonardo-Lima@users.noreply.github.com>
2025-10-08 01:32:17 -0300

    Update Exercicio8.c
    Remoções exedentes

--- Capitulo1/Comandos de Entrada e Saída/Exercicio8.c
+++ Capitulo1/Comandos de Entrada e Saída/Exercicio8.c
@@ -2,7 +2,6 @@
 
 Descrição: Esse programa converte a idade de anos de vida para dias de vida e 
 exibe o resultado usando a função printf.
-Nome: Lucas Garcia e Leonardo de Oliveira
 Data: 19/09/2025
 
 *******************************************************************************/
