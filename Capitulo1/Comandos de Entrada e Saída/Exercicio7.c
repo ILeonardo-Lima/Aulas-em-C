5867f902df2e471f448f0ac0a23b0a9ff6f38e71
Leonardo Oliveira <110206541+ILeonardo-Lima@users.noreply.github.com>
2025-10-08 01:35:42 -0300

    Update Exercicio7.c

--- Capitulo1/Comandos de Entrada e Saída/Exercicio7.c
+++ Capitulo1/Comandos de Entrada e Saída/Exercicio7.c
@@ -2,6 +2,7 @@
 
 Descrição: Esse programa faz a conversão de um valor em polegadas para centímetros 
 e exibe o seu valor usando a função printf.
+Nome:Leonardo de Oliveira
 Data: 19/09/2025
 
 *******************************************************************************/
