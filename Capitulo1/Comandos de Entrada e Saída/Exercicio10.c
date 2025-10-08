50c92846c2296ec195d0c718407508503beeaac1
Leonardo Oliveira <110206541+ILeonardo-Lima@users.noreply.github.com>
2025-10-08 01:29:22 -0300

    Update Exercicio10.c
    Remoções exedentes

--- Capitulo1/Comandos de Entrada e Saída/Exercicio10.c
+++ Capitulo1/Comandos de Entrada e Saída/Exercicio10.c
@@ -2,7 +2,6 @@
 
 Descrição: Esse programa lê 4 notas de um aluno, calcula a média entre elas e
 exibe o seu resultado usando a função printf.
-Nome: Lucas Garcia e Leonardo de Oliveira
 Data: 19/09/2025
 
 *******************************************************************************/
